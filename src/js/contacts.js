bfe40986c9ffb693f22c8d684b81d68db1f7158c
Alex Pogorelov <alexey.intertech@gmail.com>
2017-02-06 19:21:52 +0200

    contacts map scrolling

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,56 +1,32 @@
-(function() {
+var map;
+function initMap() {
 
-    function jsonToList($el, json, type) {
-        // configure type
-        var i = 0;
-        $el.empty();
-        json = json.split(',');
-        if (!type) {
-            for (i = 0; i < json.length; i++) {
-                $('<li>').text(
-                    $.trim(json[i])
-                ).appendTo($el);
-            }
-        } else if (type === 'email') {
-            for (i = 0; i < json.length; i++) {
-                $('<li>').text(
-                    $.trim(json[i])
-                ).appendTo($el);
-            }
-        } else if (type === 'phone') {
-            for (i = 0; i < json.length; i++) {
-                $('<li>').text(
-                    $.trim(json[i])
-                ).appendTo($el);
-            }
-        } else if (type === 'skype') {
-            for (i = 0; i < json.length; i++) {
-                $('<li>').text(
-                    $.trim(json[i])
-                ).appendTo($el);
-            }
+    var mapWidth = $('#map').width(),
+        centerMap = mapWidth / 4;
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 49.4053761, lng: 32.1087626 },
+        zoom: 12,
+        styles: [{ "featureType": "poi.business", "stylers": [{ "visibility": "off" }] }, { "featureType": "poi.school", "stylers": [{ "visibility": "off" }] }, {}, { "featureType": "landscape", "stylers": [{ saturation: -100 }] }, { "featureType": "whater", "stylers": [{ saturation: -100 }] }]
+    });
+    if (map && markers && markers.length) {
+        for (var i = 0; i < markers.length; i++) {
+            var markerCurrent = markers[i];
+            var markerApi = new google.maps.Marker({
+                position: { lat: markerCurrent[2], lng: markerCurrent[3] },
+                map: map,
+                title: markerCurrent[0]
+            });
         }
+        map.panBy(-centerMap, 0);
     }
 
-    function set($el, data) {
-        $el.text(data);
-    }
-
-    function moveMap(lat, lng) {}
-
-    var buttons = $('.js-button-map-trigger'),
-        dataName = $('.js-button-map-name'),
-        dataAddress = $('.js-button-map-address'),
-        dataCellphones = $('.js-button-map-cellphones'),
-        dataEmails = $('.js-button-map-emails');
-    buttons.on('click', function() {
-        var $this = $(this);
-        $this.addClass('active').siblings().removeClass('active');
-
-        set(dataName, $this.data('name'));
-        set(dataAddress, $this.data('address'));
-        jsonToList(dataCellphones, $this.data('cellphones'));
-        jsonToList(dataEmails, $this.data('emails'));
+    $('#map-addresses-accordion').on('show.bs.collapse', function (e) {
+        var $target = $(e.target),
+            lat = $target.data('lat'),
+            lng = $target.data('lng');
+        map.setCenter({lat: lat, lng: lng});
+        map.panBy(-centerMap, 0);
     });
 
-})();
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,26 +2,6 @@ if ($.browser.mobile) $('body').addClass('mobile');
 if ($.browser.safari) $('body').addClass('safari');
 if ($.browser.iphone || $.browser.ipad || $.browser.ipod) $('body').addClass('ios');
 
-var map;
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 49.4053761, lng: 32.1087626 },
-        zoom: 12,
-        styles: [{ "featureType": "poi.business", "stylers": [{ "visibility": "off" }] }, { "featureType": "poi.school", "stylers": [{ "visibility": "off" }] }, {}, { "featureType": "landscape", "stylers": [{ saturation: -100 }] }, { "featureType": "whater", "stylers": [{ saturation: -100 }] }]
-    });
-    if (map && markers && markers.length) {
-        for (var i = 0; i < markers.length; i++) {
-            var markerCurrent = markers[i];
-            var markerApi = new google.maps.Marker({
-                position: { lat: markerCurrent[2], lng: markerCurrent[3] },
-                map: map,
-                title: markerCurrent[0]
-            });
-        }
-    }
-}
-
 $('article, .wysiwyg').find('table').each(function() {
     $(this).wrap('<div class="table-holder"></div>');
 });
